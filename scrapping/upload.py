efc574659c066766f429c3eb1dbfb07c171cf2fc
YourUsername <you@example.com>
2025-07-19 14:18:01 +0300

    actions

---
+++ scrapping/upload.py
@@ -0,0 +1,18 @@
+import requests
+
+with open("token.txt") as f:
+    token = f.read()
+    
+with open("banners.json") as f :
+    content = f.read()
+
+owner = "godisdeadLOL"
+repo = "ba-schedule"
+
+headers = {"Authorization": f"Bearer {token}"}
+
+response = requests.post(
+    f"https://api.github.com/repos/{owner}/{repo}/actions/workflows/main.yaml/dispatches",
+    headers=headers,
+    json={"ref": "main", "inputs": {"bannersData": content}}
+)
